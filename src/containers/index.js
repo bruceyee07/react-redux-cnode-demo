86398a360b7fff298e4206fa17d314d8c33b2949
yehao <yehao@lianjia.com>
2017-06-29 11:57:09 +0800

    启用babel-preset-stage-0

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route } from 'react-router-dom'
 import configureStore from '../stores'
-import AsyncApp from './asyncApp'
+import HomePage from '../components/HomePage'
 import TopicContent from '../components/TopicContent'
 
 const store = configureStore()
@@ -13,7 +13,7 @@ export default class Root extends Component {
       <Provider store={store}>
       	<BrowserRouter>
       		<div>
-      			<Route path="/" component={AsyncApp} />
+      			<Route path="/" component={HomePage} />
       			<Route path="/topic/:id" component={TopicContent} />
       		</div>
       	</BrowserRouter>
